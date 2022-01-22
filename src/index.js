07e35e559e73e6add2267024f628bc2b430b4873
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-01-23 01:50:26 +0200

    add debounce and styling

--- src/index.js
+++ src/index.js
@@ -11,37 +11,43 @@ const refs = {
   countryInfo: document.querySelector('.country-info'),
 };
 
-refs.input.addEventListener('input', onSearchCountry);
+refs.input.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
 
-function onSearchCountry(event) {
-  const name = event.currentTarget.value;
-  fetchCountries(name.trim())
-    .then(arrCountries => {
-      renderCountriesList(arrCountries);
-      if (arrCountries.length > 10) {
+function onSearchCountry() {
+  const name = refs.input.value.trim();
+
+  if (name) {
+    fetchCountries(name)
+      .then(arrCountries => {
+        if (arrCountries.length > 10) {
+          refs.countryList.innerHTML = '';
+          refs.countryInfo.innerHTML = '';
+          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else {
+          renderCountriesList(arrCountries);
+          if (arrCountries.length === 1) {
+            refs.countryList.innerHTML = '';
+            renderCountriesInfo(arrCountries);
+          } else {
+            refs.countryInfo.innerHTML = '';
+          }
+        }
+      })
+      .catch(error => {
         refs.countryList.innerHTML = '';
         refs.countryInfo.innerHTML = '';
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      } else if (arrCountries.length === 1) {
-        renderCountriesInfo(arrCountries);
-      } else {
-        refs.countryInfo.innerHTML = '';
-      }
-    })
-    .catch(error => {
-      refs.countryList.innerHTML = '';
-      refs.countryInfo.innerHTML = '';
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }
 }
 
 function renderCountriesList(arrCountries) {
   const markup = arrCountries
     .map(({ flags, name }) => {
-      return `<li class='country-list'>
+      return `<li class='country-item'>
           <div class='country-shortinfo'>
           <img src = '${flags.svg}', alt = '${name.official}'></img>
-          <p><b>${name.official}</b></p>
+          <p class="country-name">${name.official}</p>
           </div>
           </li>
         `;
@@ -52,35 +58,17 @@ function renderCountriesList(arrCountries) {
 
 function renderCountriesInfo(arrCountries) {
   const markup = arrCountries
-    .map(({ capital, population, languages }) => {
-      return `<li class='country-list'>
-           <p><b>Capital:</b>${capital}</p>
-          <p><b>Population:</b>${population}</p>
-           <p><b>Languages:</b>${Object.values(languages)}</p>
-           </li>
+    .map(({ capital, population, languages, flags, name }) => {
+      return `<div class="country-shortinfo">
+      <img src = '${flags.svg}', alt = '${name.official}'></img>
+          <h2 class="country-title">${name.official}</h2></div>
+           <p class="country-prop">Capital:<span class="country-prop-value">${capital}</span></p>
+          <p class="country-prop">Population:<span class="country-prop-value">${population}</span></p>
+           <p class="country-prop">Languages:<span class="country-prop-value">${Object.values(
+             languages,
+           )}</span></p>
         `;
     })
     .join('');
   refs.countryInfo.innerHTML = markup;
-}
-
-// function renderCountries(countries) {
-//   const markup = countries
-//     .map(({ name, capital, population, flags, languages }) => {
-//       return `<li>
-//           <p><img>${flags.svg}</img><h2>${name.official}</h2></p>
-//           <p><b>Capitel:</b>${capital}</p>
-//           <p><b>Population:</b>${population}</p>
-//           <p><b>Languages:</b>${Object.values(languages)}</p>
-//           </li>
-//         `;
-//     })
-//     .join('');
-//   refs.countryList.innerHTML = markup;
-// }
-
-function emptyInput(event) {
-  if (!event.currentTarget.value.trim()) {
-    return (refs.countryList.innerHTML = '');
-  }
 }
